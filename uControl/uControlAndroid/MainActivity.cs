de1fda4d99898f5356d053221c293732cd98924d
Тараскин Константин <mrtaracqk@gmail.com>
2017-03-11 23:16:51 +0300

    UC // rework UI

--- uControl/uControlAndroid/MainActivity.cs
+++ uControl/uControlAndroid/MainActivity.cs
@@ -3,13 +3,13 @@ using Android.App;
 using Android.Content;
 using Android.Widget;
 using Android.OS;
-using Andrule.Views;
 using uControlAndroid;
+using Andrule.Views;
 
 namespace Andrule
 {
     [Activity(Label = "Andrule", MainLauncher = true, ScreenOrientation = Android.Content.PM.ScreenOrientation.Landscape)]
-    public class MainActivity : TabActivity
+    public class MainActivity : Activity
     {
         public static TabHost Tabs { get; private set;}
 
@@ -17,24 +17,33 @@ namespace Andrule
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
-            Tabs = TabHost;
+            var setupBtn = FindViewById(Resource.Id.openSetupBtn);
+            setupBtn.Click += delegate {
+                var intent = new Intent(this, typeof(SetupActivity));
+				StartActivity(intent);
+            };
 
-            CreateTab(typeof(SetupActivity), "setup", "SETUP");
-            CreateTab(typeof(WheelActivity), "wheel", "WHEEL");
+            var wheelBtn = FindViewById(Resource.Id.openWheelBtn);
+            wheelBtn.Click += delegate {
+                var intent = new Intent(this, typeof(WheelActivity));
+                StartActivity(intent);
+            };
+            //Tabs = TabHost;
+
+            //CreateTab(typeof(SetupActivity), "setup", "SETUP");
+            //CreateTab(typeof(WheelActivity), "wheel", "WHEEL");
         }
 
-        private void CreateTab(Type activityType, string tag, string label)
-        {
-            var intent = new Intent(this, activityType);
-            intent.AddFlags(ActivityFlags.NewTask);
+   //     private void CreateTab(Type activityType, string tag, string label)
+   //     {
+   //         var intent = new Intent(this, activityType);
+   //         intent.AddFlags(ActivityFlags.NewTask);
 
-            var spec = Tabs.NewTabSpec(tag);
-			spec.SetIndicator(label);
-            spec.SetContent(intent);
+   //         var spec = Tabs.NewTabSpec(tag);
+			//spec.SetIndicator(label);
+   //         spec.SetContent(intent);
 
-            Tabs.AddTab(spec);
-        }
+   //         Tabs.AddTab(spec);
+   //     }
     }
 }
-
-
