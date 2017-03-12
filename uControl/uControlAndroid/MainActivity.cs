4ff691c5d859861b10635a336837c0c151bff9b5
Salakhov Artur <gsmilexd@gmail.com>
2017-03-12 08:30:47 +0300

    update logic for gamePad service

--- uControl/uControlAndroid/MainActivity.cs
+++ uControl/uControlAndroid/MainActivity.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using Android.App;
+﻿using Android.App;
 using Android.Content;
 using Android.Widget;
 using Android.OS;
 using uControlAndroid;
 using Andrule.Views;
 using uControlAndroid.Services;
-using uControlAndroid.Entities;
-using uControlAndroid.Common;
 
 namespace Andrule
 {
@@ -33,47 +30,9 @@ namespace Andrule
             wheelBtn.Click += delegate
             {
                 var intent = new Intent(this, typeof(WheelActivity));
-                StartActivity(intent);                
+                StartActivity(intent);
             };
+        }        
 
-            GamePadServiceTest();
-        }
-
-        private void GamePadServiceTest()
-        {
-            GamePadService = new GamePadService();
-
-            var gamePadList = GamePadService.GetGamePadList();
-
-            GamePadService.CreateOrUpdateGamePad(new ListItem { Name = "asas" });
-            gamePadList = GamePadService.GetGamePadList();
-
-            var newControl = new Control
-            {
-                GamePadId = gamePadList[0].Id,
-                ControlType = ControlType.Accelerometer,
-                Height = 11,
-                Weight = 22,
-                Id = 0,
-                XPos = 100,
-                YPos = 200
-            };
-
-            newControl.Id = GamePadService.CreateOrUpdateControl(newControl);
-
-            var contr = GamePadService.GetGamePadControls(newControl.GamePadId);
-
-            GamePadService.DeleteControl(newControl.GamePadId, newControl.Id);
-
-            contr = GamePadService.GetGamePadControls(newControl.GamePadId);
-
-            GamePadService.DeleteGamePad(gamePadList[0].Id);
-
-            gamePadList = GamePadService.GetGamePadList();
-
-            GamePadService.CreateOrUpdateGamePad(new ListItem { Name = "asas1" });
-
-            gamePadList = GamePadService.GetGamePadList();
-        }
     }
 }
